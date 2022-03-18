aed134c9e56e6f9558049579a8479c69917cb19c
binoypilakkat <binoypilakkat@admaren.com>
2022-03-18 17:25:50 +0530

    Fixed bugs

--- timer.py
+++ timer.py
@@ -1,23 +1,23 @@
 from microbit import display, running_time, sleep
 from microbit import button_a, button_b, Image
-sel0 = True
-sel1 = False
-cursel = "W"
+
 w_min = [1, 10, 15, 30, 45, 60] # Timer intervals
-bool_timer = False
+
 w_times = [(v * 60) for v in w_min]
-i = 0
+i = -1 # Time Selection index
 t0 = 0
 tval = 0
-if sel0:
-    display.scroll("Press A to start", wait=False)
+
+display.scroll("Press A to start", wait=False)
 timer_started = False
-show_img = True
+
 imgs = [Image.CLOCK12, Image.CLOCK11, Image.CLOCK10, 
     Image.CLOCK9, Image.CLOCK8, Image.CLOCK7, Image.CLOCK6, 
     Image.CLOCK5, Image.CLOCK4, Image.CLOCK3, Image.CLOCK2,
     Image.CLOCK1]
-im = 0
+
+N_IMGS = len(imgs) - 1
+im = 0 
 
 timer_over = False
 
@@ -32,27 +32,21 @@ def anim():
                     
                     return 
 
-
-
 while 1:
     if timer_over is True:
         display.clear()
         timer_started = False
         anim()
         timer_over = False
-    if timer_started is True:
+    if timer_started:
         curtime = round((running_time() - t0)/(1000), 2)
-        if i == 0:
-            _i = 0
-        else:
-            _i = i - 1
-            
-        if curtime > w_times[_i]:
-            t0 = 0
+                    
+        if curtime > w_times[i]:
             timer_started = False
             timer_over = True
-        if show_img is True:
-            display.show(imgs[len(imgs) - 1 - im], wait=False)
+            show_img = False
+        if show_img:
+            display.show(imgs[N_IMGS - im], wait=False)
             sleep(100)
             if im == len(imgs) - 1:
                 im = 0
@@ -60,26 +54,28 @@ while 1:
                 im += 1
  
     if button_a.was_pressed():
-        if timer_started is False:
-            display.scroll(w_min[i])
-            if i == len(w_min) - 1:
+
+        if not timer_started:
+            if i == len(w_min) - 1: #increment index
                 i = 0
             else:
                 i += 1 
+            
+            display.scroll(w_min[i], delay=80)
         else:  # timer is running
-            if i == 0:
-                _i = 0
-            else:
-                _i = i-1
-            display.scroll(w_times[_i])
+            display.scroll(w_min[i])
     
     if button_b.was_pressed():
+        if i < 0:
+             i = 0
         if timer_started is False:
+            display.scroll("St.. : %i m"%w_min[i], delay=60)
             t0 = running_time()
             timer_started = True
+            show_img = True
         else:
             
             cur_min = round(curtime,2)//60
             cur_sec = round(curtime,2)-(cur_min*60)
-            display.scroll("%im %is"%(cur_min, cur_sec), delay=90, wait=True)
+            display.scroll("%im %is"%(cur_min, cur_sec), delay=80, wait=True)
             show_img= True
